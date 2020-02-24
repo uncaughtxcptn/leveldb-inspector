0cdd2c94896e21e398d73fbe77864a442eb079bb
Arnelle Balane <arnellebalane@gmail.com>
2020-02-24 23:55:46 +0700

    Build main and renderer as multiple webpack targets

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,57 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
+const mode = isProd ? 'production' : 'development';
 
-module.exports = {
-  entry: {
-    main: './src/main.js',
-    rendered: './src/renderer.js'
+const commonRules = [
+  {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    exclude: /node_modules/,
+    options: {
+      presets: ['@babel/preset-env']
+    }
+  }
+];
+
+module.exports = [
+  {
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js'
+    },
+    target: 'node',
+    externals: nodeExternals(),
+    mode,
+    module: {
+      rules: commonRules
+    }
   },
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    publicPath: '/'
-  },
-
-  mode: isProd ? 'production' : 'development',
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-env']
+  {
+    entry: './src/renderer.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'renderer.js',
+      publicPath: '/'
+    },
+    mode,
+    module: {
+      rules: [
+        ...commonRules,
+        {
+          test: /\.vue$/,
+          use: 'vue-loader'
+        },
+        {
+          test: /\.css$/,
+          use: ['vue-style-loader', 'css-loader']
         }
-      },
-      {
-        test: /\.vue$/,
-        use: 'vue-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ['vue-style-loader', 'css-loader']
-      }
-    ]
-  },
-
-  plugins: [new VueLoaderPlugin()]
-};
+      ]
+    },
+    plugins: [new VueLoaderPlugin()]
+  }
+];
