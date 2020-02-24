7a9fdf3425b1e80d43bff6289928254dee8ff689
Arnelle Balane <arnellebalane@gmail.com>
2020-02-24 23:57:49 +0700

    Rename source files; Add vue to dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -16,42 +16,42 @@ const commonRules = [
   }
 ];
 
-module.exports = [
-  {
-    entry: './src/main.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'main.js'
-    },
-    target: 'node',
-    externals: nodeExternals(),
-    mode,
-    module: {
-      rules: commonRules
-    }
+const mainProcessConfig = {
+  entry: './src/main-process.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main-process.js'
   },
-
-  {
-    entry: './src/renderer.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'renderer.js',
-      publicPath: '/'
-    },
-    mode,
-    module: {
-      rules: [
-        ...commonRules,
-        {
-          test: /\.vue$/,
-          use: 'vue-loader'
-        },
-        {
-          test: /\.css$/,
-          use: ['vue-style-loader', 'css-loader']
-        }
-      ]
-    },
-    plugins: [new VueLoaderPlugin()]
+  target: 'node',
+  externals: nodeExternals(),
+  mode,
+  module: {
+    rules: commonRules
   }
-];
+};
+
+const rendererProcessConfig = {
+  entry: './src/renderer-process.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'renderer-process.js',
+    publicPath: '/'
+  },
+  mode,
+  module: {
+    rules: [
+      ...commonRules,
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [new VueLoaderPlugin()]
+};
+
+module.exports = [mainProcessConfig, rendererProcessConfig];
