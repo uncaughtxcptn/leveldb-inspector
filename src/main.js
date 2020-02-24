6b0be7f4c40731a59a93e9bdb680829ddcbf6fd4
Arnelle Balane <arnellebalane@gmail.com>
2020-02-24 23:56:02 +0700

    Add initial code for main and renderer processes

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from 'electron';
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 800
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
