8aa9075e40634654b6b35ff97077df0d874b4305
Arnelle Balane <arnellebalane@gmail.com>
2020-03-25 00:33:49 +0800

    Initialize new Vuex store

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const state = {};
+
+const mutations = {};
+
+const actions = {};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions
+});
